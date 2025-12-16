9b53d147eafd548bdc05eb416493de650ceb0a86
niles <nileshmkjee123@gmail.com>
2025-12-16 21:09:06 +0530

    Piston API added for code execution

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
+import path from "path";
+import axios from 'axios';
 const app = express()
 
-import path from "path";
+
 const server = http.createServer(app)
 
 const port = process.env.PORT || 5000
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-    }
+  cors: {
+    origin: "*",
+  }
 })
 
 const rooms = new Map()
@@ -64,6 +66,27 @@ io.on("connection", (socket) => {
 
   socket.on("languageChange", ({ roomId, language }) => {
     io.to(roomId).emit("languageUpdate", language);
+  });
+
+  socket.on("compileCode", async ({ code, roomId, language, version }) => {
+    if (rooms.has(roomId)) {
+      const room = rooms.get(roomId);
+      const response = await axios.post(
+        "https://emkc.org/api/v2/piston/execute",
+        {
+          language,
+          version,
+          files: [
+            {
+              content: code,
+            },
+          ],
+        }
+      );
+
+      room.output = response.data.run.output;
+      io.to(roomId).emit("codeResponse", response.data);
+    }
   });
 
   socket.on("disconnect", () => {
